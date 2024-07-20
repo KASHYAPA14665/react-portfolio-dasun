bf00027851f623313e960e0f239d6af38a681db0
Dasun Kashyapa Bandara <dasunkashyapa20@gmail.com>
2024-07-20 22:30:12 +0530

    Updated the  navbar list items link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,13 +9,13 @@ const Navbar = () => {
   return (
     <nav className="mb-5 flex items-center justify-between py-6">
         <div className="flex relative flex-shrink-0 items-center">
-            <a href="#"><h2 className="font-bold mx-2 text-3xl ">Portfolio</h2></a>
+            <a href="#"><h2 className="font-bold mx-2 text-3xl hover:text-purple-600 transition-all delay-150">Portfolio</h2></a>
         </div>
         <div className="flex  p-2 items-center justify-center gap-4 text-2xl">
-            <a href="https://www.linkedin.com/in/dasun-kashyapa-a554a71a4/"><FaLinkedin /></a>
-            <a href="#"><FaGithub /></a>
-            <a href="#"><FaFacebook /></a>
-            <a href="#"><GoMail /></a>
+            <a href="https://www.linkedin.com/in/dasun-kashyapa-a554a71a4/" className="text-neutral-100 hover:text-purple-600 transition-all delay-150"><FaLinkedin /></a>
+            <a href="https://github.com/KASHYAPA14665" className="text-neutral-100hover:text-purple-600 transition-all delay-150 hover:text-purple-600 transition-all delay-150" ><FaGithub /></a>
+            <a href="https://www.facebook.com/profile.php?id=100086312091125&mibextid=kFxxJD" className=" text-neutral-100 hover:text-purple-600 transition-all delay-150"><FaFacebook /></a>
+            <a href="mailto:dasunkashyapa20@gmail.com" className="text-neutral-100 hover:text-purple-600 transition-all delay-150"><GoMail /></a>
         </div>
         
     </nav>
